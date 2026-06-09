47ba2d6ee9821688ef576b83dc82a65586b79005
Andrew Dean <andrew.dean@chainguard.dev>
2026-06-09 10:24:30 +1000

    fix typo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from CBA Workshop!', status: 'ok' });
+  res.json({ message: 'Hello from Container Workshop!', status: 'ok' });
 });
 
 app.get('/health', (req, res) => {
